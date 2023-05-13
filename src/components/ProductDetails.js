288f4d2ac2f4842d3f9d689e5a9e29a6eda81351
sawfiz <dashu.up@gmail.com>
2023-05-13 17:08:46 +0800

    Add styling

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -27,26 +27,28 @@ export default function ProductDetails({inventory, addToCart }) {
   }
 
   return (
-    <div className={styles.productContainer}>
-      <div className={styles.imgContainer}><img src={item.image} alt="Orbeez" /></div>
-      <div className={styles.details}>
-        <div className={styles.bold}>{item.name}</div>
-        <div>{item.description}</div>
-        <div>Price ${item.price.toFixed(2)}</div>
-        <div>
-          <button onClick={decrementQty}>-</button>
-          <input
-            value={qty || '0'}
-            size="3"
-            type="text"
-            pattern="[0-9]*"
-            onChange={(e) => updateQty(e)}
-          />
-          <button onClick={incrementQty}>+</button>
+    <div className={styles.productDetails}>
+      <div className={styles.productContainer}>
+        <div className={styles.imgContainer}><img src={item.image} alt="Orbeez" /></div>
+        <div className={styles.details}>
+          <div className={styles.bold}>{item.name}</div>
+          <div>{item.description}</div>
+          <div>Price ${item.price.toFixed(2)}</div>
+          <div>
+            <button onClick={decrementQty}>-</button>
+            <input
+              value={qty || '0'}
+              size="3"
+              type="text"
+              pattern="[0-9]*"
+              onChange={(e) => updateQty(e)}
+            />
+            <button onClick={incrementQty}>+</button>
+          </div>
+          <button onClick={() => addToCart({ item: item, qty: qty })}>
+            Add to cart
+          </button>
         </div>
-        <button onClick={() => addToCart({ item: item, qty: qty })}>
-          Add to cart
-        </button>
       </div>
     </div>
   );
