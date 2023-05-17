d4ca827110a96d195cf7c92e2330159ed40cafe1
sawfiz <dashu.up@gmail.com>
2023-05-17 21:37:10 +0800

    Add localStorage for cart

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,10 +1,19 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import cartReducer from '../reducers/cartReducer';
 
 export const CartContext = createContext();
 
 export function CartContextProvider(props) {
-  const [cart, dispatch] = useReducer(cartReducer,[]);
+  const [cart, dispatch] = useReducer(cartReducer,[], () => {
+    const localData = localStorage.getItem('cart');
+    return localData ? JSON.parse(localData) : []
+  });
+
+    // This runs every time books change, e.g. add and remove books
+    useEffect(
+      () => localStorage.setItem('cart', JSON.stringify(cart)),
+      [cart]
+    );
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
